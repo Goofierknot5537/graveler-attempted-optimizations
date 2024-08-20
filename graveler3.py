dc84244f53ea3d86415a94fd8baa7eecdf394d6e
Goofierknot5537 <62623578+Goofierknot5537@users.noreply.github.com>
2024-08-20 17:02:02 -0400

    Add files via upload

---
+++ graveler3.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+numbers = [0,0,0,0]
+rolls = 100000
+maxOnes = 0
+
+random = np.random.randint(0,4, size=(231 * rolls))
+
+divided_random = np.array_split(random, rolls)
+
+for array in divided_random:
+    numbers = np.bincount(array)
+    if numbers[0] > maxOnes:
+        maxOnes = numbers[0]
+
+numbers = [0,0,0,0]
+
+print("Highest Ones Roll:",maxOnes)
+print("Number of Roll Sessions: ",rolls)
