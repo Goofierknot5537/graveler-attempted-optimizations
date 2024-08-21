bf9c7d71e1793a3ac72fdee20cd017e252d98546
Goofierknot5537 <62623578+Goofierknot5537@users.noreply.github.com>
2024-08-21 19:11:35 -0400

    Update graveler7.py
    Looked kinda weird

--- graveler7.py
+++ graveler7.py
@@ -8,7 +8,7 @@ sub_rolls = -(rolls // -10000000)
 
 maxNum = 0
 for i in range(sub_rolls):
-    random = rng.binomial(n = 231, p=0.25, size = -(rolls // -sub_rolls))
+    random = rng.binomial(n=231, p=0.25, size= -(rolls // -sub_rolls))
     maximum = np.max(random)
     if maximum > maxNum:
         maxNum = maximum
