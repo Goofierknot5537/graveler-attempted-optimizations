89e3412885e25d1754c7d42d0854c2fa392f5d13
Goofierknot5537 <62623578+Goofierknot5537@users.noreply.github.com>
2024-08-21 14:34:29 -0400

    Add files via upload

---
+++ graveler7.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+rng = np.random.default_rng()
+
+rolls = 10000000
+
+sub_rolls = rolls // 10000000
+
+maxNum = 0
+for i in range(sub_rolls):
+    random = rng.binomial(n = 231, p=0.25, size = -(rolls // -sub_rolls))
+    maximum = np.max(random)
+    if maximum > maxNum:
+        maxNum = np.max(random)
+
+print(f"Highest Roll: {maxNum}\n"
+      f"Number of Roll Sessions: {rolls}")
